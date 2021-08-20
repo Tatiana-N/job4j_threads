9c093161d0d100545e3d5029267bb9942109c42b
Tatiana-N <tanuskanikolaeva@mail.ru>
2021-08-20 07:41:07 +0300

    Thread state

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+	public static void main(String[] args) {
+		Thread first = new Thread(
+				() -> {}
+		);
+		Thread second = new Thread(
+				() -> {}
+		);
+		System.out.println(first.getState()+ " " + first.getName());
+		System.out.println(second.getState()+ " " + second.getName());
+		first.start();
+		second.start();
+		while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED ) {
+			System.out.println(first.getState() + " " + first.getName());
+			System.out.println(second.getState()+ " " + second.getName());
+		}
+		System.out.println(first.getState()+ " " + first.getName());
+		System.out.println(second.getState()+ " " + second.getName());
+		System.out.println(Thread.currentThread().getName() + " завершила выполнение программы");
+	}
+}
