6ef4b656d19fb474b0241db79b3df53ec0a4511a
Tatiana-N <tanuskanikolaeva@mail.ru>
2021-08-25 06:46:33 +0300

    Thread stop 2

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,18 +1,24 @@
 package ru.job4j.concurrent;
 
+import java.util.Arrays;
+
 public class ConsoleProgress implements Runnable {
+	String [] strings = new String[]{"\\","|","/","-"};
 	@Override
 	public void run() {
 		try {
-			Thread.sleep(500);
 			while (!Thread.currentThread().isInterrupted()) {
-				System.out.print("\r load: " + "\\");
-				System.out.print("\r load: " + "|");
-				System.out.print("\r load: " + "/");
-				System.out.print("\r load: " + "-");
+				Thread.sleep(500);
+				Arrays.stream(strings).forEach(string ->System.out.print("\r load: " + string));
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+	public static void main(String[] args) throws InterruptedException {
+		Thread progress = new Thread(new ConsoleProgress());
+		progress.start();
+		Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+		progress.interrupt();
 	}
 }
