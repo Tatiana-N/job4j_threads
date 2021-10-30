30b5c11a98a7be9a03201e5aad5d5a4df7c2249c
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2021-10-30 14:36:28 +0300

    Thread без общих ресурсов [#267919] №2

---
+++ README.md
@@ -0,0 +1,3 @@
+# job4j_threads
+[![Build Status](https://app.travis-ci.com/Tatiana-N/job4j_threads.svg?branch=master)](https://app.travis-ci.com/Tatiana-N/job4j_threads)
+[![codecov](https://codecov.io/gh/Tatiana-N/job4j_threads/branch/master/graph/badge.svg?token=3w0FBS6VRF)](https://codecov.io/gh/Tatiana-N/job4j_threads)
--- src/main/java/ru/job4j/concurrent/UserCache.java
+++ src/main/java/ru/job4j/concurrent/UserCache.java
@@ -1,10 +1,9 @@
 package ru.job4j.concurrent;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
@@ -17,9 +16,14 @@ public class UserCache {
 	}
 	
 	public User findById(int id) {
-		return users.get(id);
+		return User.of(users.get(id).getName());
 	}
 	public List<User> findAll() {
-		return new CopyOnWriteArrayList(users.values());
+		List<User> usersList = new ArrayList<>();
+		Enumeration<User> elements = users.elements();
+		while (elements.hasMoreElements()){
+		usersList.add(User.of(elements.nextElement().getName()));
+		}
+		return usersList;
 	}
 }
