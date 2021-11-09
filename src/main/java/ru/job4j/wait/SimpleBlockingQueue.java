a5e5f58c9ca663e2a3e11bff2ef0c7b926a75bec
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2021-11-10 00:37:19 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/wait/SimpleBlockingQueue.java
@@ -0,0 +1,34 @@
+package ru.job4j.wait;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+	
+	@GuardedBy("this")
+	private final Queue<T> queue = new LinkedList<>();
+	
+	public synchronized void offer(T value) {
+		queue.add(value);
+		notifyAll();
+	}
+	
+	public synchronized T poll() {
+		while (queue.isEmpty()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+		return queue.poll();
+	}
+	
+	public synchronized Queue<T> getQueue() {
+		return queue;
+	}
+}
---
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.wait;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleBlockingQueueTest {
+	@Test
+	void offer() throws InterruptedException {
+		SimpleBlockingQueue<Double> simpleBlockingQueue = new SimpleBlockingQueue<>();
+		List<Thread> threadList = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			threadList.add(new Thread(() -> simpleBlockingQueue.offer(Math.random())));
+		}
+		threadList.forEach(Thread::start);
+		
+		for (Thread thread : threadList) {
+			thread.join();
+		}
+		Assertions.assertEquals(10, simpleBlockingQueue.getQueue().size());
+	}
+	
+	@Test
+	void poll() throws InterruptedException {
+		SimpleBlockingQueue<Double> simpleBlockingQueue = new SimpleBlockingQueue<>();
+		List<Thread> threadList = new ArrayList<>();
+		for (int i = 0; i < 10000; i++) {
+			threadList.add(new Thread(() -> simpleBlockingQueue.offer(Math.random())));
+			threadList.add(new Thread(simpleBlockingQueue::poll));
+		}
+		threadList.forEach(Thread::start);
+		for (Thread thread : threadList) {
+			thread.join();
+		}
+		Assertions.assertEquals(0, simpleBlockingQueue.getQueue().size());
+	}
+}
