ad14474e7c18eef18ef06d3d3ee0fb00146c0825
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2021-11-17 13:19:11 +0300

    3. ForkJoinPool [#315067] 2

--- src/main/java/ru/job4j/pool/ParallelSearching.java
+++ src/main/java/ru/job4j/pool/ParallelSearching.java
@@ -4,55 +4,47 @@ import java.util.concurrent.RecursiveTask;
 
 
 public class ParallelSearching extends RecursiveTask<Integer> {
-	
-	int start;
-	int finish;
-	Object[] objects;
-	Object o;
-	
-	public ParallelSearching(Object[] objects, int start, int finish, Object o) {
-		if (o == null) {
-			throw new IllegalArgumentException("не можем искать null");
-		}
-		this.start = start;
-		this.finish = finish;
-		this.objects = objects;
-		this.o = o;
-	}
-	
-	public ParallelSearching(Object[] objects, Object o) {
-		
-		this(objects, 0, objects.length - 1, o);
-	}
-	
-	@Override
-	protected Integer compute() {
-		if (finish <= 10) {
-			return smileMassiveSearch();
-		}
-		if (start == finish) {
-			if (o.equals(objects[start])) {
-				return start;
-			} else {
-				return -1;
-			}
-		}
-		int mid = (start + finish) / 2;
-		ParallelSearching parallelSearching1 = new ParallelSearching(objects, start, mid, o);
-		ParallelSearching parallelSearching2 = new ParallelSearching(objects, mid + 1, finish, o);
-		parallelSearching1.fork();
-		parallelSearching2.fork();
-		Integer join1 = parallelSearching1.join();
-		Integer join2 = parallelSearching2.join();
-		return join1 == -1 ? join2 : join1;
-	}
-	
-	private Integer smileMassiveSearch() {
-		for (int i = 0; i < objects.length; i++) {
-			if (objects[i].equals(o)) {
-				return i;
-			}
-		}
-		return -1;
-	}
+
+  private final int start;
+  private final int finish;
+  private final Object[] objects;
+  private final Object o;
+
+  public ParallelSearching(Object[] objects, int start, int finish, Object o) {
+    if (o == null) {
+      throw new IllegalArgumentException("не можем искать null");
+    }
+    this.start = start;
+    this.finish = finish;
+    this.objects = objects;
+    this.o = o;
+  }
+
+  public ParallelSearching(Object[] objects, Object o) {
+    this(objects, 0, objects.length - 1, o);
+  }
+
+  @Override
+  protected Integer compute() {
+    if (finish - start <= 10) {
+      return smileMassiveSearch(start, finish);
+    }
+    int mid = (start + finish) / 2;
+    ParallelSearching parallelSearchingLeft = new ParallelSearching(objects, start, mid, o);
+    ParallelSearching parallelSearchingRight = new ParallelSearching(objects, mid + 1, finish, o);
+    parallelSearchingLeft.fork();
+    parallelSearchingRight.fork();
+    Integer indexLeft = parallelSearchingLeft.join();
+    Integer indexRight = parallelSearchingRight.join();
+    return indexLeft == -1 ? indexRight : indexLeft;
+  }
+
+  private Integer smileMassiveSearch(int start, int finish) {
+    for (int i = start; i <= finish; i++) {
+      if (o.equals(objects[i])) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
