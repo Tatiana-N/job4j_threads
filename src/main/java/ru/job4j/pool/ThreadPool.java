2ec17aabf9912c2d4510dad820dc1022ff54d189
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2021-11-17 17:25:20 +0300

    3. ForkJoinPool [#315067] не останавливающийся пул

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -6,46 +6,51 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ThreadPool {
-  private final List<Thread> threads = new LinkedList<>();
-  private final SimpleBlockingQueue<Runnable> tasks;
+	private final List<Thread> threads = new LinkedList<>();
+	private final SimpleBlockingQueue<Runnable> tasks;
 
-  public ThreadPool() {
-    int size = Runtime.getRuntime().availableProcessors();
-    tasks = new SimpleBlockingQueue<>(size);
-    for (int i = 0; i < size; i++) {
-      threads.add(new Thread(() -> {
-        while (!Thread.currentThread().isInterrupted()) {
-          try {
-            tasks.poll().run();
-          } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-          }
-        }
-      }));
-    }
-    threads.forEach(Thread::start);
-  }
+	public ThreadPool() {
+		int size = Runtime.getRuntime().availableProcessors();
+		 tasks= new SimpleBlockingQueue<>(size);
+		for (int i = 0; i < size; i++) {
+			threads.add(new Thread(() -> {
+				while (!Thread.currentThread().isInterrupted()) {
+					try {
+						tasks.poll().run();
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+					}
+				}
+			}));
+		}
+		threads.forEach(Thread::start);
+	}
 
-  public void work(Runnable runnable) {
-    try {
-      tasks.offer(runnable);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-  }
+	public void work(Runnable runnable) {
+		try {
+			tasks.offer(runnable);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-  public void shoutDown() {
-    threads.forEach(Thread::interrupt);
-  }
+	public void shoutDown() {
+		threads.forEach(Thread::interrupt);
+	}
 
-  public static void main(String[] args) throws InterruptedException {
-    ThreadPool threadPool = new ThreadPool();
-    threadPool.work(() -> System.out.println("task1"));
-    threadPool.work(() -> System.out.println("task2"));
-    Thread.sleep(2000);
-    threadPool.work(() -> System.out.println("task3"));
-    Thread.sleep(1000);
-    threadPool.work(() -> System.out.println("task4"));
-    threadPool.shoutDown();
-  }
+	public static void main(String[] args) throws InterruptedException {
+		ThreadPool threadPool = new ThreadPool();
+		for (int i = 0; i < 20; i++) {
+			int finalI = i;
+			threadPool.work(() -> {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("task" + finalI);
+			});
+		}
+		threadPool.shoutDown();
+	}
 }
