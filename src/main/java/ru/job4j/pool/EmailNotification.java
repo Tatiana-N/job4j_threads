4a029586f363847a77876d2cd8ab2ec31432688c
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2021-11-17 11:36:16 +0300

    2. ExecutorService рассылка почты. [#63097] 2

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -5,19 +5,26 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-	private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-	
-	public void emailTo(User user) {
-		pool.submit(() -> send(
-				String.format("Notification %s to email %s", user.getUserName(), user.getEmail()),
-				String.format("Add a new event to %s", user.getUserName()),
-				user.getEmail()));
-	}
-	
-	public void close() {
-		pool.shutdown();
-	}
-	
-	public void send(String subject, String body, String email) {
-	}
+  private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+  public void emailTo(User user) {
+    pool.submit(() -> send(
+      String.format("Notification %s to email %s", user.getUserName(), user.getEmail()),
+      String.format("Add a new event to %s", user.getUserName()),
+      user.getEmail()));
+  }
+
+  public void close() {
+    pool.shutdown();
+    while (!pool.isTerminated()) {
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void send(String subject, String body, String email) {
+  }
 }
