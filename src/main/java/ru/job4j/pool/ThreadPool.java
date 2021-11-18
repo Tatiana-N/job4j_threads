53794b555aede26e27bf8af97fa92aa3e3f3ca74
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2021-11-18 23:36:37 +0300

    1. Реализовать ThreadPool [#1099] вернула без переменной

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -8,14 +8,13 @@ import java.util.List;
 public class ThreadPool {
   private final List<Thread> threads = new LinkedList<>();
   private final SimpleBlockingQueue<Runnable> tasks;
-  private volatile boolean isAlive = true;
 
   public ThreadPool() {
     int size = Runtime.getRuntime().availableProcessors();
     tasks = new SimpleBlockingQueue<>(size);
     for (int i = 0; i < size; i++) {
       threads.add(new Thread(() -> {
-        while (isAlive) {
+        while (!Thread.currentThread().isInterrupted()) {
           try {
             tasks.poll().run();
           } catch (InterruptedException e) {
@@ -27,31 +26,31 @@ public class ThreadPool {
     threads.forEach(Thread::start);
   }
 
-  public void work(Runnable runnable) {
-    try {
-      tasks.offer(runnable);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+  public void work(Runnable runnable) throws InterruptedException {
+    tasks.offer(runnable);
   }
 
   public void shoutDown() {
     threads.forEach(Thread::interrupt);
-    isAlive = false;
   }
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) {
     ThreadPool threadPool = new ThreadPool();
     for (int i = 0; i < 20; i++) {
       int finalI = i;
-      threadPool.work(() -> {
-        try {
-          Thread.sleep(1000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        System.out.println("task" + finalI);
-      });
+      try {
+        threadPool.work(() -> {
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+          System.out.println("task" + finalI);
+        });
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+        Thread.currentThread().interrupt();
+      }
     }
     threadPool.shoutDown();
   }
