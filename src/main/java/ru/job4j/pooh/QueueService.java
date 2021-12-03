2d70cd3af507199834431d12a75d194a7e671782
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2021-12-03 20:46:45 +0300

    Pooh Service еще рефакторинг

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -12,14 +12,17 @@ public class QueueService implements Service {
 	public Response process(Request request) {
 		ConcurrentLinkedQueue<String> strings = new ConcurrentLinkedQueue<>();
 		ConcurrentLinkedQueue<String> result = topics.putIfAbsent(request.getSourceName(), strings);
-		if (request.getParam() != null) {
-			strings.add(request.getParam());
-		}
+		
+		Response response = result == null
+				? new Response("", "200")
+				: result.isEmpty() ? new Response("", "203")
+				: new Response(result.poll(), "200");
+		
 		if ("POST".equals(request.httpRequestType())) {
 			Optional.ofNullable(result).ifPresent(res -> res.add(request.getParam()));
+			strings.add(request.getParam());
+			response = new Response("", "200");
 		}
-		return result != null && !result.isEmpty()
-				? new Response(result.poll(), "200")
-				: new Response("", result != null ? "203" : "200");
+		return response;
 	}
 }
--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -9,26 +9,30 @@ public class TopicService implements Service {
 	
 	@Override
 	public Response process(Request request) {
-		
-		ConcurrentLinkedQueue<String> queue = null;
+		Response response;
 		ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> map = new ConcurrentHashMap<>();
 		map.put(request.getParam(), new ConcurrentLinkedQueue<>());
 		ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> subscribers = topics.putIfAbsent(request.getSourceName(), map);
 		
 		switch (request.httpRequestType()) {
 			case "GET":
-				queue = subscribers != null ? subscribers.putIfAbsent(request.getParam(), new ConcurrentLinkedQueue<>()) : null;
+				ConcurrentLinkedQueue<String>	queue = subscribers != null ? subscribers.putIfAbsent(request.getParam(), new ConcurrentLinkedQueue<>()) : null;
+				response =
+						queue == null
+						? new Response("", "200")
+						: queue.isEmpty() ? new Response("", "203")
+						: new Response(queue.poll(), "200");
 				break;
+				
 			case "POST":
 				if (subscribers != null) {
 					subscribers.values().forEach(que -> que.add(request.getParam()));
+					response = new Response("", "200");
 					break;
 				}
 			default:
-				return new Response("", "203");
+				response = new Response("", "203");
 		}
-		return queue != null && !queue.isEmpty()
-				? new Response(queue.poll(), "200")
-				: new Response("", queue != null ? "203" : "200");
+		return response;
 	}
 }
